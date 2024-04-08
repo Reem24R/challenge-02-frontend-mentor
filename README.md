ac2e7a53dbdb2e3b3c82cb81c07662c1943229c5
reem <reem2004mohamed.com>
2024-04-08 09:48:24 +0200

    challenge-02

---
+++ README.md
@@ -0,0 +1,7 @@
+# Frontend Mentor - FAQ accordion
+
+![Design preview for the FAQ accordion coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+
---
+++ script.js
@@ -0,0 +1,44 @@
+function show1() {
+    let pr = document.getElementsByClassName('p2')[0];
+    let img3=document.getElementById('img1');
+    if (pr.style.display === 'none' || pr.style.display === '') {
+        pr.style.display = 'block';
+        img3.src='./assets/images/icon-minus.svg';
+      } else {
+        pr.style.display = 'none';
+        img3.src='./assets/images/icon-plus.svg';
+      }
+  }
+function show2() {
+    let pr = document.getElementsByClassName('p2')[1];
+    let img3=document.getElementById('img2');
+    if (pr.style.display === 'none' || pr.style.display === '') {
+        pr.style.display = 'block';
+        img3.src='./assets/images/icon-minus.svg';
+      } else {
+        pr.style.display = 'none';
+        img3.src='./assets/images/icon-plus.svg';
+      }
+  }
+function show3() {
+    let pr = document.getElementsByClassName('p2')[2];
+    let img3=document.getElementById('img3');
+    if (pr.style.display === 'none' || pr.style.display === '') {
+      pr.style.display = 'block';
+      img3.src='./assets/images/icon-minus.svg';
+    } else {
+      pr.style.display = 'none';
+      img3.src='./assets/images/icon-plus.svg';
+    }
+  }
+function show4() {
+    let pr = document.getElementsByClassName('p2')[3];
+    let img3=document.getElementById('img4');
+    if (pr.style.display === 'none' || pr.style.display === '') {
+        pr.style.display = 'block';
+        img3.src='./assets/images/icon-minus.svg';
+      } else {
+        pr.style.display = 'none';
+        img3.src='./assets/images/icon-plus.svg';
+      }
+  }
